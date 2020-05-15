cebda202f999f625495618c6035842e6373505fa
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 09:29:40 -0600

    Add second test for project show

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -12,5 +12,11 @@ RSpec.describe "project's show page" do
   end
 
   it "should also display the theme of the challenge it belongs to" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("Recycled Material")
   end
 end
