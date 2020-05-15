47c7b40cece5d98ec318a64b4c7fafa5982574a8
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 10:16:55 -0600

    Add test for new method in project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it "#average_years_experience" do
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+      expect(upholstery_tux.average_years_experience).to eq(10)
+    end
+  end
 end
