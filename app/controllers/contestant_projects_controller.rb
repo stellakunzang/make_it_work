0c5edfca872dcda53e889bf890cc27d42b0a4ea9
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 10:47:25 -0600

    Add create method to contestant project controller

---
+++ app/controllers/contestant_projects_controller.rb
@@ -0,0 +1,10 @@
+class ContestantProjectsController < ApplicationController
+
+  def create
+    ContestantProject.create({
+      contestant_id: params[:contestant_id],
+      project_id: params[:id]
+      })
+      redirect_to "/projects/#{params[:id]}"
+  end
+end
