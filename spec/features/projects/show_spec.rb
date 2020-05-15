cf155e804bae0f9362627aad7cf9498964b658ec
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 09:20:41 -0600

    Add test for project show page

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "project's show page" do
+  it "should display the project's name and material" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(new_chic.name)
+    expect(page).to have_content(new_chic.material)
+  end
+
+  it "should also display the theme of the challenge it belongs to" do
+  end
+end
