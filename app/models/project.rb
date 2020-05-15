cb51adc9d11780633e28d8b065c335822e418d81
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-15 10:14:19 -0600

    Add average_years_experience method to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,9 @@ class Project <ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+
+  def average_years_experience
+    contestants.average(:years_of_experience)
+  end
 end
